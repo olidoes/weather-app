0f3920542a128939a2d7f465ca322e75ac7ff2cf
Oli <palmbusiness12@gmail.com>
2026-01-31 09:45:48 +0000

    Fully styled website. Added some basic click handling functionality in javascript.

---
+++ app.js
@@ -0,0 +1,11 @@
+async function getWeatherData(location) {}
+
+// This function handles the click received from the Event Listener for the check button
+function handleCheckButtonClick() {
+    const valueOfInputField = document.getElementById("input-area").value;
+}
+
+// This detects the clicking of the check button below the input field
+document
+    .getElementById("input-button")
+    .addEventListener("click", handleCheckButtonClick);
